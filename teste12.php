40ef4f8ad93b30c22a0e9abd7f2b407ba9a36341
Nicolas Campos Francisco <112672297+NicolasCampos012@users.noreply.github.com>
2024-09-18 17:20:12 -0300

    Add files via upload

---
+++ teste12.php
@@ -0,0 +1,15 @@
+<?php
+
+for (;;) {
+    if ($i>50) {
+        break;
+    }
+    
+    echo "$i ...";
+    $i++;
+
+/* O código abaixo apresenta o uso do comando for. 
+É utilizado um laço infinito, porém quando a variável $i for maior de 50, o laço será encerrado. */
+
+}
+?>
