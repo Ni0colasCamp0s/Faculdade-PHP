0ce6cbba34cc4d204c7bbbe6aac4a17aa6eb99f6
Nicolas Campos Francisco <112672297+Ni0colasCamp0s@users.noreply.github.com>
2024-10-10 14:40:41 -0300

    Add files via upload

---
+++ Teste16.php
@@ -0,0 +1,12 @@
+<?php
+
+$status = TRUE;
+if ($status) {
+    function valida() {
+        echo "Função valdiada!";
+    }
+}
+
+/*Corrigir*/
+
+?>
---
+++ Teste17.php
@@ -0,0 +1,13 @@
+<?php 
+
+function quadrado ($valor) {
+    echo "Quadrado de $valor é: " . ($valor*$valor);
+}
+quadrado (4);
+
+/* No código é apresentado um exemplo de função que recebe um argumento por parâmetro. 
+A função quadrado () recebe um argumento ($valor) por parâmetro.
+Ao chamar a função quadrado (), o argumento quatro é enviado por parâmetro. 
+Este argumento é recebido pela função e utilizado para determinar o quadrado do argumento ($valor*$valor).*/
+
+?>
